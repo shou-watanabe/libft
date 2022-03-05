719b4760731507d2483b8a37b5192c8634b539f5
shou-watanabe <watanabeshou1520@gmail.com>
2022-03-06 02:39:57 +0900

    :sparkles: ft_isprint

--- libft/srcs/ft_isprint.c
+++ libft/srcs/ft_isprint.c
@@ -1,22 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isprint.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: shwatana <shwatana@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/06 01:51:00 by shwatana          #+#    #+#             */
-/*   Updated: 2022/03/06 02:39:19 by shwatana         ###   ########.fr       */
+/*   Created: 2022/03/06 02:37:59 by shwatana          #+#    #+#             */
+/*   Updated: 2022/03/06 02:38:55 by shwatana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-
-int	ft_isalpha(char *c);
-int	ft_isdigit(char c);
-int	ft_isalnum(char c);
-int	ft_isascii(char c);
-int	ft_isprint(char c);
-
-#endif
+int	ft_isprint(char c)
+{
+	return (' ' <= c && c <= '~');
+}
