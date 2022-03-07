6ad0eabf0678ccc680442fcad6c38e3a0548fbca
shou-watanabe <watanabeshou1520@gmail.com>
2022-03-08 02:19:21 +0900

    :sparkles: プロトタイプ追加

--- libft/includes/libft.h
+++ libft/includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: shwatana <shwatana@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/06 01:51:00 by shwatana          #+#    #+#             */
-/*   Updated: 2022/03/06 09:50:50 by shwatana         ###   ########.fr       */
+/*   Updated: 2022/03/08 02:18:15 by shwatana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,7 @@ int		ft_isalnum(char c);
 int		ft_isascii(char c);
 int		ft_isprint(char c);
 size_t	ft_strlen(const char *s);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
 
 #endif
