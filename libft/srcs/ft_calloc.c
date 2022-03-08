e6ed1b9cbc883748cd546df35c35752f2038a12b
shou-watanabe <watanabeshou1520@gmail.com>
2022-03-08 09:37:34 +0900

    :sparkles: ft_calloc

--- libft/srcs/ft_calloc.c
+++ libft/srcs/ft_calloc.c
@@ -1,29 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: shwatana <shwatana@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/06 01:51:00 by shwatana          #+#    #+#             */
-/*   Updated: 2022/03/08 08:23:39 by shwatana         ###   ########.fr       */
+/*   Created: 2022/03/08 08:22:21 by shwatana          #+#    #+#             */
+/*   Updated: 2022/03/08 09:37:08 by shwatana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <stdlib.h>
+void	*ft_calloc(size_t num, size_t size)
+{
+	void	*buf;
 
-void	ft_bzero(void *s, size_t n);
-void	*ft_calloc(size_t num, size_t size);
-int		ft_isalnum(char c);
-int		ft_isalpha(char *c);
-int		ft_isascii(char c);
-int		ft_isdigit(char c);
-int		ft_isprint(char c);
-size_t	ft_strlen(const char *s);
-int		ft_tolower(int c);
-int		ft_toupper(int c);
-
-#endif
+	buf = (void *)malloc(size * num);
+	if (buf)
+		ft_bzero(buf, num * size);
+	return (buf);
+}
