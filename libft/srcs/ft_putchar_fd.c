0a86cc556a1f6e7e42e4ef2d9ff93b19182e10e8
shou-watanabe <watanabeshou1520@gmail.com>
2022-03-11 23:46:42 +0900

    :memos: include追加

--- libft/srcs/ft_putchar_fd.c
+++ libft/srcs/ft_putchar_fd.c
@@ -6,9 +6,11 @@
 /*   By: shwatana <shwatana@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/11 23:38:17 by shwatana          #+#    #+#             */
-/*   Updated: 2022/03/11 23:39:11 by shwatana         ###   ########.fr       */
+/*   Updated: 2022/03/11 23:44:00 by shwatana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 void	ft_putchar_fd(char c, int fd)
 {
