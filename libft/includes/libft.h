96e5f0e2c73d538abbca44204803f4c73814af3f
shou-watanabe <watanabeshou1520@gmail.com>
2022-03-11 23:40:43 +0900

    :sparkles: ft_putchar_fd

--- libft/includes/libft.h
+++ libft/includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: shwatana <shwatana@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/06 01:51:00 by shwatana          #+#    #+#             */
-/*   Updated: 2022/03/08 16:28:09 by shwatana         ###   ########.fr       */
+/*   Updated: 2022/03/11 23:40:23 by shwatana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,9 @@ int		ft_isdigit(int c);
 int		ft_isprint(int c);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 // void	*ft_memccpy(void *dst, void *src, int c, size_t n);
-void	*ft_memmove(void *dst, const void *src, size_t size);
+// void	*ft_memmove(void *dst, const void *src, size_t size);
 void	*ft_memset(void *buf, int c, size_t size);
+void	ft_putchar_fd(char c, int fd);
 size_t	ft_strlen(const char *s);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
