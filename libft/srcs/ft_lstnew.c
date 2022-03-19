6e961ee0f9cafa892e190cd99e90450867634a50
shou-watanabe <watanabeshou1520@gmail.com>
2022-03-19 21:21:52 +0900

    WIP

---
+++ libft/srcs/ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shwatana <shwatana@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/12 10:26:37 by shwatana          #+#    #+#             */
+/*   Updated: 2022/03/16 10:23:36 by shwatana         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include "libft.h"
+
+// t_list	*ft_lstnew(void *content)
+// {
+// 	t_list	*new_list;
+
+// 	new_list = malloc(sizeof(t_list));
+// 	if (new_list == NULL)
+// 		return (NULL);
+// 	new_list->next = NULL;
+// 	new_list->content = content;
+// 	return (new_list);
+// }
