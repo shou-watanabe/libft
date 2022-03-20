2f93ddb24439ec6973b708cc977c040f7308ef45
shou-watanabe <watanabeshou1520@gmail.com>
2022-03-20 15:15:50 +0900

    :memos: 引数修正

--- libft/srcs/ft_lstdelone.c
+++ libft/srcs/ft_lstdelone.c
@@ -6,13 +6,13 @@
 /*   By: shwatana <shwatana@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/20 14:53:09 by shwatana          #+#    #+#             */
-/*   Updated: 2022/03/20 14:56:25 by shwatana         ###   ########.fr       */
+/*   Updated: 2022/03/20 15:11:09 by shwatana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstdelone(t_list **lst, void (*del)(*void))
+void	ft_lstdelone(t_list *lst, void (*del)(*void))
 {
 	if (lst == NULL || *lst == NULL)
 		return ;
