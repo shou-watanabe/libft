ceed3fbe9e5111d3e2c0c3770f68e7c7a0491231
shou-watanabe <watanabeshou1520@gmail.com>
2022-03-20 14:48:11 +0900

    :memos: norm修正

--- libft/srcs/ft_lstadd_front.c
+++ libft/srcs/ft_lstadd_front.c
@@ -6,13 +6,13 @@
 /*   By: shwatana <shwatana@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/20 14:38:07 by shwatana          #+#    #+#             */
-/*   Updated: 2022/03/20 14:41:05 by shwatana         ###   ########.fr       */
+/*   Updated: 2022/03/20 14:47:19 by shwatana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_lstadd_front(t_list **lst, t_list *new)
+void	ft_lstadd_front(t_list **lst, t_list *new)
 {
 	t_list	*lst_last;
 
--- libft/srcs/ft_lstlast.c
+++ libft/srcs/ft_lstlast.c
@@ -6,13 +6,13 @@
 /*   By: shwatana <shwatana@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/20 11:57:29 by shwatana          #+#    #+#             */
-/*   Updated: 2022/03/20 12:01:42 by shwatana         ###   ########.fr       */
+/*   Updated: 2022/03/20 14:47:50 by shwatana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list *ft_lstlast(t_list *lst)
+t_list	*ft_lstlast(t_list *lst)
 {
 	if (lst == NULL)
 		return (NULL);
