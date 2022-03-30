a73aa6867d8a48c5f002213127a721b356e2cc2d
shou-watanabe <watanabeshou1520@gmail.com>
2022-03-30 10:57:45 +0900

    :memos: NULL handling

--- srcs/ft_putstr_fd.c
+++ srcs/ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: shwatana <shwatana@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/11 23:43:23 by shwatana          #+#    #+#             */
-/*   Updated: 2022/03/11 23:45:02 by shwatana         ###   ########.fr       */
+/*   Updated: 2022/03/30 08:25:30 by shwatana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	ft_putstr_fd(char *str, int fd)
 {
 	int	len;
 
+	if (str == NULL)
+		return ;
 	len = ft_strlen(str);
 	write(fd, str, len);
 }
