1f6b6b28f8f1135b7c3fc89e9bd05e497b5a1c19
shou-watanabe <watanabeshou1520@gmail.com>
2022-04-01 22:54:10 +0900

    :wastebasket: comment

--- srcs/ft_strtrim.c
+++ srcs/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: shwatana <shwatana@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/17 10:34:05 by shwatana          #+#    #+#             */
-/*   Updated: 2022/03/17 13:19:38 by shwatana         ###   ########.fr       */
+/*   Updated: 2022/04/01 22:53:05 by shwatana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,10 +27,3 @@ char	*ft_strtrim(const char *s1, const char *set)
 	printf("%zu\n", len);
 	return (ft_substr(s1, 0, len));
 }
-
-// int	main(void)
-// {
-// 	char	*str = "   Hello World!   ";
-
-// 	printf("%s\n", ft_strtrim(str, " "));
-// }
