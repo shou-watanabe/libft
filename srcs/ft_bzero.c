e895bae6d1c05a7c66cd2fc2130062cf24e94289
shou-watanabe <watanabeshou1520@gmail.com>
2022-04-06 13:24:13 +0900

    :memos: use ft_memset

--- srcs/ft_bzero.c
+++ srcs/ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: shwatana <shwatana@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/08 02:19:59 by shwatana          #+#    #+#             */
-/*   Updated: 2022/03/08 09:50:37 by shwatana         ###   ########.fr       */
+/*   Updated: 2022/04/06 13:09:54 by shwatana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,5 @@
 
 void	ft_bzero(void *s, size_t n)
 {
-	size_t	i;
-	char	*str;
-
-	str = (char *)s;
-	i = 0;
-	while (i < n)
-	{
-		str[i] = '\0';
-		i++;
-	}
+	ft_memset(s, '\0', n);
 }
