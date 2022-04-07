f730838d9c339cd23b3330806796f4a671b13454
shou-watanabe <watanabeshou1520@gmail.com>
2022-04-07 14:11:49 +0900

    :sparkles: add test code

--- srcs/ft_strncmp.c
+++ srcs/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: shwatana <shwatana@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 21:28:17 by shwatana          #+#    #+#             */
-/*   Updated: 2022/04/01 22:13:02 by shwatana         ###   ########.fr       */
+/*   Updated: 2022/04/07 14:11:42 by shwatana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,3 +34,18 @@ int	ft_strncmp(const char *s1, const char *s2, size_t size)
 	}
 	return (diff);
 }
+
+// int main(void)
+// {
+// 	puts("----ft_strncmp------");
+//     char str141[] = "chidoridaisuki";
+//     char str142[] = "chidoridaisuki";
+//     char str143[] = "chidori&nobu&daigo";
+//     printf("library: %d\n", strncmp(str141, str142, 14));
+//     printf("zisaku : %d\n", ft_strncmp(str141, str142, 14));
+//     printf("library: %d\n", strncmp(str141, str143, 7));
+//     printf("zisaku : %d\n", ft_strncmp(str141, str143, 7));
+//     printf("library: %d\n", strncmp(str141, str143, 8));
+//     printf("zisaku : %d\n", ft_strncmp(str141, str143, 8));
+// 	return (0);
+// }
