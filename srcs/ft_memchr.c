ac3d9e08ccd20ec38a2bc22e7044f0e6bec1804e
shou-watanabe <watanabeshou1520@gmail.com>
2022-04-07 19:02:39 +0900

    :memos: comment out

--- srcs/ft_memchr.c
+++ srcs/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: shwatana <shwatana@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 23:33:30 by shwatana          #+#    #+#             */
-/*   Updated: 2022/04/06 14:04:05 by shwatana         ###   ########.fr       */
+/*   Updated: 2022/04/07 19:02:01 by shwatana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	i = 0;
 	cchr = (unsigned char)c;
 	schr = (unsigned char *)s;
+	if (n == 0)
+		return (NULL);
 	while (i < n)
 	{
 		if (schr[i] == cchr)
@@ -29,3 +31,12 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	}
 	return (NULL);
 }
+
+// int main(void)
+// {
+// 	char str51[] ="nesugosita!!!";
+//     char str52[] ="nesugosita!!!";
+//     printf("zisaku  : %s\n", (char *)ft_memchr(str51, 103, 15));
+//     printf("library : %s\n", (char *)memchr(str52, 103, 15));
+// 	return (0);
+// }
