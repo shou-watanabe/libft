15bd0d562993e2c428785731dba239749c22c3f1
shou-watanabe <watanabeshou1520@gmail.com>
2022-04-07 17:39:48 +0900

    :memos: comment out

--- srcs/ft_isalpha.c
+++ srcs/ft_isalpha.c
@@ -6,7 +6,7 @@
 /*   By: shwatana <shwatana@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/06 01:50:43 by shwatana          #+#    #+#             */
-/*   Updated: 2022/04/07 14:59:24 by shwatana         ###   ########.fr       */
+/*   Updated: 2022/04/07 17:19:42 by shwatana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,14 @@ int	ft_isalpha(int c)
 	return (is_upper_alpha || is_lower_alpha);
 }
 
-int main()
-{
-	puts("-------ft_alpha----------");
-    printf("library :%d\n", isalpha('2'));
-    printf("zisaku  :%d\n", ft_isalpha('2'));
-    printf("library :%d\n", isalpha('A'));
-    printf("zisaku  :%d\n", ft_isalpha('A'));
-    printf("library :%d\n", isalpha('/'));
-    printf("zisaku  :%d\n", ft_isalpha('/'));
-	return (0);
-}
+// int main()
+// {
+// 	puts("-------ft_alpha----------");
+//     printf("library :%d\n", isalpha('2'));
+//     printf("zisaku  :%d\n", ft_isalpha('2'));
+//     printf("library :%d\n", isalpha('A'));
+//     printf("zisaku  :%d\n", ft_isalpha('A'));
+//     printf("library :%d\n", isalpha('/'));
+//     printf("zisaku  :%d\n", ft_isalpha('/'));
+// 	return (0);
+// }
