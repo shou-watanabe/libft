2a57783d1b2ea9c97841b107ed1182515755a73f
shou-watanabe <watanabeshou1520@gmail.com>
2022-04-09 14:40:35 +0900

    :memos: null handling

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: shwatana <shwatana@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/17 10:34:05 by shwatana          #+#    #+#             */
-/*   Updated: 2022/04/07 00:30:34 by shwatana         ###   ########.fr       */
+/*   Updated: 2022/04/09 14:38:28 by shwatana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ char	*ft_strtrim(const char *s1, const char *set)
 {
 	size_t	len;
 
-	if (s1 == NULL)
+	if (s1 == NULL || set == NULL)
 		return (NULL);
 	while (*s1 && ft_strchr(set, *s1))
 		s1++;
@@ -25,3 +25,10 @@ char	*ft_strtrim(const char *s1, const char *set)
 		len--;
 	return (ft_substr(s1, 0, len));
 }
+
+// int main(void)
+// {
+// 	printf("%s\n", ft_strtrim(" 1  Hello World 1  ", " 1"));
+// 	printf("%s\n", ft_strtrim(" 1  Hello World 1  ", NULL));
+// 	return (0);
+// }
