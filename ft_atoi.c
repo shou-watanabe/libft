c017643e180e0c31deb31d4525cd686c8f37a1d0
shou-watanabe <watanabeshou1520@gmail.com>
2022-04-12 09:08:59 +0900

    :memos: 命名修正

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: shwatana <shwatana@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/17 01:14:17 by shwatana          #+#    #+#             */
-/*   Updated: 2022/04/07 18:59:16 by shwatana         ###   ########.fr       */
+/*   Updated: 2022/04/12 09:08:34 by shwatana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ static int	ft_isspace(int c)
 	return (c == ' ' || (c >= '\t' && c <= '\r'));
 }
 
-static int	over_num(bool is_regular_num)
+static int	over_num(bool is_positive_num)
 {
-	if (is_regular_num == 1)
+	if (is_positive_num == 1)
 		return (-1);
 	else
 		return (0);
@@ -30,25 +30,25 @@ int	ft_atoi(const char *str)
 	long long	num;
 	long long	lm_div;
 	long long	lm_mod;
-	bool		is_regular_num;
+	bool		is_positive_num;
 
 	num = 0;
-	is_regular_num = true;
+	is_positive_num = true;
 	lm_div = LONG_MAX / 10;
 	lm_mod = LONG_MAX % 10;
 	while (ft_isspace(*str))
 		str++;
 	if (*str == '+' || *str == '-')
 		if (*str++ == '-')
-			is_regular_num = false;
+			is_positive_num = false;
 	while (ft_isdigit(*str))
 	{
 		if (num > lm_div || (num == lm_div && *str - '0' > lm_mod))
-			return (over_num(is_regular_num));
+			return (over_num(is_positive_num));
 		num = num * 10 + *str - '0';
 		str++;
 	}
-	if (!is_regular_num)
+	if (!is_positive_num)
 		num *= -1;
 	return ((int)num);
 }
