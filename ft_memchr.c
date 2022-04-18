195a86a7caaff1bfd60bde5b02b0b03b3ac7ca09
shou-watanabe <watanabeshou1520@gmail.com>
2022-04-18 09:12:14 +0900

    :memo: early return

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: shwatana <shwatana@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 23:33:30 by shwatana          #+#    #+#             */
-/*   Updated: 2022/04/07 19:02:01 by shwatana         ###   ########.fr       */
+/*   Updated: 2022/04/18 09:10:41 by shwatana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,11 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	unsigned char	*schr;
 	size_t			i;
 
+	if (n == 0)
+		return (NULL);
 	i = 0;
 	cchr = (unsigned char)c;
 	schr = (unsigned char *)s;
-	if (n == 0)
-		return (NULL);
 	while (i < n)
 	{
 		if (schr[i] == cchr)
@@ -34,8 +34,9 @@ void	*ft_memchr(const void *s, int c, size_t n)
 
 // int main(void)
 // {
-// 	char str51[] ="nesugosita!!!";
-//     char str52[] ="nesugosita!!!";
+// 	memchr(NULL, '1', 0);
+// 	char str51[] = "nesugosita!!!";
+// 	char str52[] ="nesugosita!!!";
 //     printf("zisaku  : %s\n", (char *)ft_memchr(str51, 103, 15));
 //     printf("library : %s\n", (char *)memchr(str52, 103, 15));
 // 	return (0);
