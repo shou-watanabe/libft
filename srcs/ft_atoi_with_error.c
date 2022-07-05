7c4efb0562d25f8ffda5b0b210cf66d524fde7a9
shwatanap <watanabeshou1520@gmail.com>
2022-07-05 16:12:10 +0900

    :memo: update

--- srcs/ft_atoi_with_error.c
+++ srcs/ft_atoi_with_error.c
@@ -6,21 +6,7 @@
 /*   By: shwatana <shwatana@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/05 15:43:53 by shwatana          #+#    #+#             */
-/*   Updated: 2022/07/05 15:46:29 by shwatana         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "libft.h"
-
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_atoi.c                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: shwatana <shwatana@student.42tokyo.jp>     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/17 01:14:17 by shwatana          #+#    #+#             */
-/*   Updated: 2022/04/22 17:21:54 by shwatana         ###   ########.fr       */
+/*   Updated: 2022/07/05 16:11:49 by shwatana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,29 +19,29 @@ static int	ft_isspace(int c)
 
 bool	ft_atoi_with_error(const char *str, int *num)
 {
-	long long	tmp_num;
-	long long	lm_div;
-	long long	lm_mod;
-	bool		is_positive_num;
+	int		tmp;
+	int		im_div;
+	int		im_mod;
+	bool	is_posi;
 
-	tmp_num = 0;
-	is_positive_num = true;
-	lm_div = LONG_MAX / 10;
-	lm_mod = LONG_MAX % 10;
+	tmp = 0;
+	is_posi = true;
+	im_div = INT_MAX / 10;
+	im_mod = INT_MAX % 10;
 	while (ft_isspace(*str))
 		str++;
 	if (*str == '+' || *str == '-')
 		if (*str++ == '-')
-			is_positive_num = false;
+			is_posi = false;
 	while (ft_isdigit(*str))
 	{
-		if (tmp_num > lm_div || (tmp_num == lm_div && *str - '0' > lm_mod))
+		if (tmp > im_div || (tmp == im_div && *str - '0' > im_mod + (!is_posi)))
 			return (false);
-		tmp_num = tmp_num * 10 + *str - '0';
+		tmp = tmp * 10 + *str - '0';
 		str++;
 	}
-	if (!is_positive_num)
-		tmp_num *= -1;
-	*num = tmp_num;
+	if (!is_posi)
+		tmp *= -1;
+	*num = tmp;
 	return (true);
 }
